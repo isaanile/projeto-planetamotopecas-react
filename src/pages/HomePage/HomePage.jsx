3c08c07def1d495aa736d300d17d12a4f5c8867c
Isabella Anile <isabellaanilealves@gmail.com>
2025-08-08 19:54:26 -0300

    adicçao de pastas e componentes

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Header from '../../components/Header/Header';
+import Hero from '../../components/Hero/Hero';
+import Categorias from '../../components/Categorias/Categorias';
+import ProdutosDestaque from '../../components/ProdutosDestaque/ProdutosDestaque';
+import QuemSomos from '../../components/QuemSomos/QuemSomos';
+import Footer from '../../components/Footer/Footer';
+import styles from './HomePage.module.css';
+
+const HomePage = () => {
+  return (
+    <div className={styles.homeContainer}>
+      <Header />
+      <main>
+        <Hero />
+        <Categorias />
+        <ProdutosDestaque />
+        <QuemSomos />
+        <Footer />
+      </main>
+    </div>
+  );
+};
+
+export default HomePage;
